31a125935fde9a76a208661be3c1db9ad4f7cfac
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-05 14:22:06 +0200

    Initial Commit

---
+++ ProfileNavigation/ProfileNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileNavigationApp.swift
+//  ProfileNavigation
+//
+//  Created by Eric on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProfileNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
